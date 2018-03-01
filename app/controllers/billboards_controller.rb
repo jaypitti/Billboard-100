077a5486fed824f3c1c511a1c49d7b8c331dbe82
Andrew Locke <alocke12992@gmail.com>
2018-03-01 15:00:52 -0700

    adds billboard CRUD

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,49 @@
 class BillboardsController < ApplicationController
+  before_action :set_billboard, only: [:show, :update, :edit, :destroy]
   def index
+    @billboards = Billboard.all
   end
 
   def show
   end
 
   def new
+    @billboard = billboard.new
   end
 
   def edit
   end
+
+  def create 
+    @billboard = Billboard.new(billboard_params)
+
+    if @billboard.save 
+      redirect_to billboard_path
+    else 
+      render :new 
+    end 
+  end 
+
+  def update 
+    if @billboard.update(billboard_params)
+      redirect_to @billboard
+    else 
+      render :edit 
+    end 
+  end 
+
+  def destroy 
+    @billboard.destroy 
+    redirect_to billboards_path 
+  end 
+
+  private 
+
+  def set_billboard 
+    @billboard = Billboard.find(params[:id])
+  end 
+
+  def billboard_params
+    params.require(:billboard).permit(:name)
+  end 
 end
