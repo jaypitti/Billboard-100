52d422321a93beed8b561125658b74c7c7a3a9d2
Tyson Au <au.tyson@gmail.com>
2018-03-01 15:06:47 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  resources :billboards
+  resources :billboards do
+    resources :songs
+  end
+  resources :artists do
+    resources :songs
+  end
 end
