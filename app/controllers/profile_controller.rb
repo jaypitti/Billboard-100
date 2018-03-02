a6817f8ff38a940161c91070534b4918bb7b3457
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-02 14:52:31 -0700

    New Files

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,13 @@
+class ProfileController < ApplicationController
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20180302210642) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.integer "duration"
